698c767746cdc732bf952b351c7d847497b699ca
maikytech <maikytech1307@gmail.com>
2018-10-10 21:38:56 -0500

    [Update] Close Btn

--- Controllers/LoginViewController.swift
+++ Controllers/LoginViewController.swift
@@ -10,28 +10,28 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBAction func closeBtn(_ sender: UIButton) {
+        
+        //Descarta la vista modal.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.view.bindKeyboard()    //Activa la animacion de subir la vista unos frames mientras el teclado este presente.
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        self.view.addGestureRecognizer(tap)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func handleTap(sender: UITapGestureRecognizer) {
+        
+        //endEditing cancela cualquier edicion que tengamos sobre la vista ¿??
+        self.view.endEditing(true)
+        
     }
-    */
-
 }
 
 
